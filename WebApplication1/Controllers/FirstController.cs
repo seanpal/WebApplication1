2b3121ae5daa05e996068f1c0c3ed1adb5955103
Seanpal <sanjoy.ict@whu.edu.cn>
2017-07-09 17:46:44 +0800

    Add project files.

---
+++ WebApplication1/Controllers/FirstController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        public ActionResult Welcome1()
+        {
+            return View();
+        }
+    }
+}
